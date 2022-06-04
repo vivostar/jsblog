dce618ac77843bbea23bf921a3bd67da21342e99
vivostar <lishuaipeng651@gmail.com>
2022-06-05 03:06:59 +0800

    init

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,14 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+      <main>
+        <p>This is a redwood js blog etude</p>
+      </main>
+    </>
+  )
+}
+
+export default AboutPage
